69fe26aa00c727ab389348c2981e8f50a2788ebd
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-25 11:20:25 -0500

    Initial version

---
+++ README.md
@@ -0,0 +1,65 @@
+# SASS Broccoli Qunit Generator
+
+This Yeoman generator will create a simple Broccoli project that comes setup for web development with SASS, ES6 modules (using Babel transpiling), QUnit (but ).
+It also will install [Yoga Sass](http://rtablada.github.io/yoga-sass), [Font Awesome](http://fontawesome.io), and [Normalize CSS](https://necolas.github.io/normalize.css/).
+
+## Installing the Generator
+
+```bash
+npm install -g broccoli-cli yo generator-qunit-broccoli
+```
+
+## Creating Projects
+
+To create a project with this generator run:
+
+```bash
+yo qunit-broccoli
+```
+
+This will ask you for your project name, and a few details to get up and started.
+
+## Running the Development Server
+
+Once the project has been created, move into the directory and then run:
+
+```bash
+npm run serve
+```
+
+The `Brocfile.js` injects live reload into HTML files in the `public` directory.
+This command is backed by `ember-cli` which will fire a reload whenever Broccoli rebuilds any trees.
+
+## Building the Project
+
+To build the project into a final production build, run:
+
+```bash
+npm run build
+```
+
+This will build the project into a `dist` directory that can be uploaded to services such as Firebase, Surge, or AWS.
+
+## Lining SASS
+
+This project comes with [SASS Lint](https://github.com/sasstools/qunit-lint) support.
+
+To run SASS lint, run the command:
+
+```bash
+npm run lint
+```
+
+The rules installed beyond the [SASS Lint](https://github.com/sasstools/qunit-lint/blob/master/lib/config/qunit-lint.yml) defaults:
+
+* Class Name Format: BEM
+* No IDs
+* No Important
+* Hex Notation: Lowercase
+* Indentation: 2 Spaces
+* Property Sort Order: SMACSS
+  - Box
+  - Border
+  - Background
+  - Text
+  - Other
---
+++ app/index.js
@@ -0,0 +1,88 @@
+var util = require('util');
+var path = require('path');
+var _ = require('lodash');
+var utils = require('keystone-utils');
+var yeoman = require('yeoman-generator');
+var wiring = require('html-wiring');
+
+var ProjectGenerator = module.exports = function ProjectGenerator(args, options, config) {
+
+  // Set utils for use in templates
+  this.utils = utils;
+
+  // Apply the Base Generator
+  yeoman.generators.Base.apply(this, arguments);
+
+  // Welcome
+  console.log('\nLet\'s add Broccoli to this Adonis project!\n');
+
+  // Import Package.json
+  this.pkg = JSON.parse(wiring.readFileAsString(path.join(__dirname, '../package.json')));
+
+};
+
+// Extends the Base Generator
+util.inherits(ProjectGenerator, yeoman.generators.Base);
+
+ProjectGenerator.prototype.project = function project() {
+  var copyDir = [
+    'resources/styles',
+    'resources/javascript',
+    'public/dist',
+  ];
+  var _this = this;
+
+  this.template('_.sass-lint.yml', '.sass-lint.yml');
+  this.copy('Brocfile.js', 'Brocfile.js');
+  this.copy('_.eslintrc', '.eslintrc');
+  this.copy('_.gitignore', '.gitignore');
+  this.copy('_.gitignore-public', 'public/dist/.gitignore');
+
+  copyDir.forEach(function(file) {
+    _this.bulkDirectory(file, file);
+  });
+
+  // This callback is fired when the generator has completed,
+  // and includes instructions on what to do next.
+  var done = _.bind(function done() {
+    console.log(
+    `------------------------------------------------
+
+    To your scripts in "package.json" add:
+
+    "assets:build": "ember build -o public/dist",
+    "assets:watch": "ember build -w -o public/dist",
+
+    Your project is ready to go!
+
+    To build your assets run "npm run assets:build"`);
+  }, this);
+
+  var deps = [
+    'font-awesome',
+    'normalize-css',
+    'whatwg-fetch',
+    'yoga-sass',
+    'broccoli',
+    'broccoli-autoprefixer',
+    'broccoli-babel-transpiler',
+    'broccoli-csso',
+    'broccoli-cli',
+    'broccoli-funnel',
+    'broccoli-inject-livereload',
+    'broccoli-merge-trees',
+    'broccoli-sass-source-maps',
+    'broccoli-sourcemap-concat',
+    'broccoli-stew',
+    'ember-cli',
+    'eslint',
+    'eslint-config-rtablada',
+    'loader.js',
+    'sass-lint',
+  ];
+
+  this.npmInstall(deps, {
+    skipMessage: true,
+    callback: done,
+  });
+};
